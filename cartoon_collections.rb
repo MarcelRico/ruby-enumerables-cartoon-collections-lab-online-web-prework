937de4731c5ff282dc82c6a9fc0628fc07ef4f1b
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 02:49:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,8 @@ def long_planeteer_calls(calls_array)
   calls_array.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food_array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_array.find{|food| cheese_types.include?(food)}
 end
