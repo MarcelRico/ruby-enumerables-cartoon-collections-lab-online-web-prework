a7a6100611d72e983521ae19c623378995cecbea
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 02:25:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def roll_call_dwarves(dwarves_array)
-  puts dwarves_array
+  i=1
+  dwarves_array.map do |dwarf| 
+    puts "#{i} *#{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
